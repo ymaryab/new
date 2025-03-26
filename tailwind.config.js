d9527aa4de29a8f115398710a240e0addf98c1a5
ymaryab <ymaryab@gmail.com>
2025-03-26 11:31:02 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        burgundy: {
+          50: '#fdf2f2',
+          100: '#fde8e8',
+          200: '#fbd5d5',
+          300: '#f8b4b4',
+          400: '#f98080',
+          500: '#f05252',
+          600: '#e02424',
+          700: '#c81e1e',
+          800: '#991b1b',
+          900: '#7f1d1d',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
